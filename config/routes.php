a11491303f1a1673f5cbb4b5bb7ae5a3c8ba597b
juan <carlos3434@hotmail.com>
2017-05-11 02:53:06 -0500

    delete route

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,3 @@
 <?php
 // Routes
-
-$app->group('/employees', function () {
-    // BeneficiariesWebService
-    $this->get('/wsdl', \App\Controllers\EmployeeController::class . ':wsdl');
-    $this->post('', \App\Controllers\EmployeeController::class . ':indexWS');
-});
+ $app->post('/employees', \App\Controllers\EmployeeController::class . ':indexWS');
